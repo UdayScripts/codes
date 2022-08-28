772de335d4ef4b0e99349bc353e44c86241fa40d
UdayScripts <110768804+UdayScripts@users.noreply.github.com>
2022-08-28 08:28:08 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Uday Affiliate</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <form action="upload.php">
+    <input type="file"
+           name="my_video">
+    <input type="submit"
+           name="submit"
+           value="Upload">
+
+           
+    
+  </form>
+  
+  
+  
+  
+  
+</body>
+</html>
